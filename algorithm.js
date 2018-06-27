3494a9451fcc287baa01c21bc05044566ba7a64e
RReddVar <lightningflash007@gmail.com>
2018-06-27 15:26:42 -0400

    Update with solution to Caesar's Cypher

--- algorithm.js
+++ algorithm.js
@@ -9,3 +9,32 @@ function titleCase(str) {
 }
 
 titleCase("WORD TO BE");
+
+// Code for Caesar's Cypher
+
+function rot13(str) { 
+	var test = [];
+		var track;
+	for (var i = 0; i < str.length; i++) {
+		var str2 = str.charCodeAt(i);
+
+		if ((str2 < 65)) {
+		      track = str2;
+		      test.push(String.fromCharCode(track));
+		    }
+
+		  else if(str2 - 13 < 65) {
+		    track = str2 + 13;
+		    test.push(String.fromCharCode(track));
+		  }
+
+		  else {
+			track = str2 -13;
+
+		    test.push(String.fromCharCode(track));
+		  }
+	}
+	 return test.join(''); 
+}
+
+rot13("SERR YBIR?");
