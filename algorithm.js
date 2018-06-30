31e1dbbdcda820f2d7102cb41d2e2caa4d278645
RReddVar <lightningflash007@gmail.com>
2018-06-30 19:46:15 -0400

    Update algorithm.js
    add slice and splice algorithm

--- algorithm.js
+++ algorithm.js
@@ -48,3 +48,21 @@ function palindrome(str) {
 }
 
 palindrome("TOM*&... HANKS");
+
+
+// using slice and splice: Use the array methods slice and splice to copy each element of the first array into the second array, in order.
+
+// Begin inserting elements at index n of the second array.
+
+// Return the resulting array. The input arrays should remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+
+var testArr = arr2.slice(0);  
+for (var i = arr1.length-1; i >= 0; i--) {	
+	  testArr.splice(n, 0, arr1[i]);
+}
+  return testArr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
